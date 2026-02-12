2a04784f637af4b0e8a47ec09516b73f1793a27e
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-02-12 22:38:02 +0300

    number is bigger than 100 or ?
    kullanıcıdan aldığı sayının 100 den büyük olup olmadığını kontrol eden program

---
+++ lesson6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subat122
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //kullanıcıdan aldığı sayının 100 den büyük olup olmadığını kontrol eden program
+            Console.WriteLine("Lütfen bir sayı giriniz:");
+            Console.Write("Sayı: ");
+            string inputTxt = Console.ReadLine();
+            int input = Convert.ToInt32(inputTxt);
+
+            string sonuc = input > 100 ? "sayı 100den büyüktür" : "sayı 100den büyük değildir";
+            Console.WriteLine(sonuc);
+
+
+
+        }
+    }
+}
