f9c39cfc87461b693b72d8ca5c5cf52795e20c62
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-02-17 20:50:20 +0300

    değer isteme tek satır
    tek satırda değer istetme kullanıcıdan

---
+++ ders2kisayazim.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subat17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan bir sayı girmesini istiyoruz ve bu sayıyı "sayi" adlı bir değişkene atıyoruz.
+            // kısa yazımı
+            Console.WriteLine("Bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine()); 
+           
+
+            
+
+
+
+        }
+    }
+}
