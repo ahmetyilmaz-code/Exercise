f025b368381b4f1a036684eb857090074da8327e
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-02-24 20:50:20 +0300

    Update notortalaması.cs
    Kullanıcının her girdiği notu kontrol ederek ilerlesin

--- notortalaması.cs
+++ notortalaması.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,56 +16,68 @@ namespace subat24
             // 80 den küçük 70 e büyük eşitse BB
             // 70 den küçük  50 e büyük eşitse CC
             // 50 den küçükse DD kaldın
-            
+
             // Kullanucu 0 ile 100 aralığı dışında bir not girdiğinde hata mesajı verelim ve tekrar not girmesini isteyelim
-            
+
+
+
             Console.WriteLine("Lütfen 3 adet not giriniz:");
             Console.Write("1. Not:");
             int not1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("2. Not:");
-            int not2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("3. Not:");
-            int not3 = Convert.ToInt32(Console.ReadLine());
 
-            double ortalama = (not1 + not2 + not3) / 3d;
-            if (not1 < 0 || not1 > 100 || not2 < 0 || not2 > 100 || not3 < 0 || not3 > 100)
+            
+
+           
+
+            if (not1 < 0 || not1 > 100)
             {
-            Console.WriteLine("Lütfen 0 ile 100 arasında bir not giriniz.");
-            }
-            else if (ortalama >= 90)
-            {
-                 Console.WriteLine($"Ortalamanız: {ortalama} Harf Notunuz: AA");               
-            }
-            else if (ortalama >= 80)
-            {
-                Console.WriteLine($"Ortalamanız: {ortalama} Harf Notunuz: BA");
-            }
-            else if (ortalama >= 70)
-            {
-                Console.WriteLine($"Ortalamanız: {ortalama} Harf Notunuz: BB");
-            }
-            else if (ortalama >= 50)
-            {
-                Console.WriteLine($"Ortalamanız: {ortalama} Harf Notunuz: CC");
+                Console.WriteLine("Lütfen 0 ile 100 arasında bir not giriniz.");
             }
             else
             {
-                Console.WriteLine($"Ortalamanız: {ortalama} Harf Notunuz: DD Kaldınız");
+                Console.Write("2. Not:");
+                int not2 = Convert.ToInt32(Console.ReadLine());
+                if (not2 < 0 || not2 > 100)
+                {
+                    Console.WriteLine("Lütfen 0 ile 100 arasında bir not giriniz.");
+                }
+                else
+                {
+                    Console.Write("3. Not:");
+                    int not3 = Convert.ToInt32(Console.ReadLine());
+                    if (not3 < 0 || not3 > 100)
+                    {
+                        Console.WriteLine("Lütfen 0 ile 100 arasında bir not giriniz.");
+                    }
+                    else
+                    {
+                        double ortalama = (not1 + not2 + not3) / 3d;
+                        if (ortalama >= 90)
+                        {
+                            Console.WriteLine("Ortalamanız: " + ortalama + " Harf Notunuz: AA");
+                        }
+                        else if (ortalama >= 80)
+                        {
+                            Console.WriteLine("Ortalamanız: " + ortalama + " Harf Notunuz: BA");
+                        }
+                        else if (ortalama >= 70)
+                        {
+                            Console.WriteLine("Ortalamanız: " + ortalama + " Harf Notunuz: BB");
+                        }
+                        else if (ortalama >= 50)
+                        {
+                            Console.WriteLine("Ortalamanız: " + ortalama + " Harf Notunuz: CC");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ortalamanız: " + ortalama + " Harf Notunuz: DD Kaldınız.");
+                        }
+                    }
+                }
+
+
             }
-
-
-
-
-
-
-
-
-
-
-
-            }
+        }
     }
+
 }
-
-
-
