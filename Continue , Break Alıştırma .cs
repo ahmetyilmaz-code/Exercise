e054db9a8f441be9ade9668e6ed6e4e78ada2365
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-17 23:07:42 +0300

    Continue , Break Alıştırma
    // Continue , Break Alıştırma         
                // kullanıcı negatif değer girene kadar sayıları toplayan ama çift sayıları toplama dahil etmeyen program.

---
+++ Continue , Break Alıştırma .cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart1701
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Continue , Break Alıştırma         
+            // kullanıcı negatif değer girene kadar sayıları toplayan ama çift sayıları toplama dahil etmeyen program.
+            int toplam = 0, i = 1;
+            while (true)
+            {
+                Console.Write($"{i}.Sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi < 0)
+                {
+                    Console.WriteLine("Negatif değer girdiğiniz için program sonlandırıldı.");
+                    break; 
+                }
+                if (sayi % 2 == 0)
+                {
+                    Console.WriteLine("Çift sayı girdiğiniz için program bu değeri yok saydı.");
+                    continue; 
+                }
+                else 
+                { 
+                    toplam += sayi; 
+                }
+                i++;
+            }
+            Console.WriteLine($"Girilen Sayıların Toplamı: {toplam}");
+        }
+    }
+}
